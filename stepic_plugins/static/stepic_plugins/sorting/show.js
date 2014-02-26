28872b3a5ad1e5cb712c14e76d52575775989cd0
matklad <aleksey.kladov@gmail.com>
2014-02-26 13:28:43 +0400

    minor sort quiz update

--- stepic_plugins/static/stepic_plugins/sorting/show.js
+++ stepic_plugins/static/stepic_plugins/sorting/show.js
@@ -1,12 +1,15 @@
 function showSortingQuiz(target, template, dataset, reply, disabled, quiz_info) {
   target.html(template(dataset));
-  var dragSource = null;
+  if (MathJax) {
+    MathJax.Hub.Queue(["Typeset", MathJax.Hub, target.get()])
+  }
 
-  target.find("li").on('dragstart', function(e){
+  var dragSource = null;
+  target.find("li").on('dragstart',function () {
     dragSource = this;
-  }).on('dragover', function(e){
+  }).on('dragover',function (e) {
     e.preventDefault();
-  }).on('drop', function(e){
+  }).on('drop', function () {
     var tmp = dragSource.innerHTML;
     dragSource.innerHTML = this.innerHTML;
     this.innerHTML = tmp;
@@ -14,10 +17,10 @@ function showSortingQuiz(target, template, dataset, reply, disabled, quiz_info) 
 
   return {
     'submit': function () {
-      ordering = target.find('.sorting-quiz__item_number')
-      .map(function() {
-        return parseInt($(this).text(), 10);
-      }).get();
+      var ordering = target.find('.sorting-quiz__item_number')
+        .map(function () {
+          return parseInt($(this).text(), 10);
+        }).get();
       return {'ordering': ordering };
     }
   };
