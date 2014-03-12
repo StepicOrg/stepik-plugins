351c6a7e60661ab5e22833034dddf4419f6346f8
matklad <aleksey.kladov@gmail.com>
2014-03-12 20:12:21 +0400

    better dataset quiz

--- stepic_plugins/quizzes/dataset/show.coffee
+++ stepic_plugins/quizzes/dataset/show.coffee
@@ -1,6 +1,23 @@
 App.DatasetQuizComponent = Em.Component.extend
+  is_dataset_downloaded: false
+
   init: ->
     @_super()
     if not @get('reply')?
       @set 'reply',
         text: ''
+
+  is_input_disabled: (->
+    @get('disabled') or not @get('is_dataset_downloaded')
+  ).property('disabled', 'is_dataset_downloaded')
+
+  didInsertElement: ->
+    @$('.get_dataset').click =>
+      @get('controller').send 'download_started'
+    # @focus()
+    @_super.apply(@, arguments)
+
+
+  actions:
+    download_started: ->
+      @set 'is_dataset_downloaded', true
--- stepic_plugins/static/stepic_plugins/dataset/show.js
+++ stepic_plugins/static/stepic_plugins/dataset/show.js
@@ -1,11 +1,27 @@
 (function() {
   App.DatasetQuizComponent = Em.Component.extend({
+    is_dataset_downloaded: false,
     init: function() {
       this._super();
       if (this.get('reply') == null) {
         return this.set('reply', {
           text: ''
         });
+      }
+    },
+    is_input_disabled: (function() {
+      return this.get('disabled') || !this.get('is_dataset_downloaded');
+    }).property('disabled', 'is_dataset_downloaded'),
+    didInsertElement: function() {
+      var _this = this;
+      this.$('.get_dataset').click(function() {
+        return _this.get('controller').send('download_started');
+      });
+      return this._super.apply(this, arguments);
+    },
+    actions: {
+      download_started: function() {
+        return this.set('is_dataset_downloaded', true);
       }
     }
   });
