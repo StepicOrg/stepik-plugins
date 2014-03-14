5f0fa8df8bcaa36d922d12ec30bfe42c84306f6a
matklad <aleksey.kladov@gmail.com>
2014-03-14 19:33:41 +0400

    remove unnecessary complicated tests

--- stepic_plugins/quizzes/simple_choice/tests.py
+++ stepic_plugins/quizzes/simple_choice/tests.py
@@ -1,96 +1,68 @@
-from stepic_plugins.base import quiz_wrapper_factory
+import unittest
+
 from stepic_plugins.exceptions import FormatError
-from stepic_plugins.tests import InitTest, GenerateTest, CleanReplyTest, CheckTest
 from . import SimpleChoiceQuiz
 
-QuizClass = quiz_wrapper_factory(SimpleChoiceQuiz)
-quiz = QuizClass({
-    'options': [
-        {'is_correct': True, 'text': 'A'},
-        {'is_correct': False, 'text': 'B'},
-    ]
-})
+
+class SimpleChoiceTest(unittest.TestCase):
+    def setUp(self):
+        self.default_source = {
+            'options': [
+                {'is_correct': True, 'text': 'A'},
+                {'is_correct': False, 'text': 'B'},
+            ]
+        }
+
+        self.quiz = SimpleChoiceQuiz(SimpleChoiceQuiz.Source(self.default_source))
 
 
-class SimpleChoiceInitTest(InitTest):
-    def specs(self):
-        return [
-            {
-                'quiz_class': QuizClass,
-                'output': FormatError,
-                '->': [
-                    {
-                        'source': {
-                            'options': [
-                                {'is_correct': True, 'text': 'A'},
-                                {'is_correct': True, 'text': 'B'},
-                            ]
-                        },
-                    },
-                    {
-                        'source': {
-                            'options': [
-                                {'is_correct': False, 'text': 'A'},
-                                {'is_correct': False, 'text': 'B'},
-                            ]
-                        },
-                    }
-                ]
-            }
-        ]
+class SimpleChoiceInitTest(SimpleChoiceTest):
+    def test_exactly_one_correct(self):
+        with self.assertRaises(FormatError):
+            SimpleChoiceQuiz(SimpleChoiceQuiz.Source({'options': [
+                {'is_correct': True, 'text': 'A'},
+                {'is_correct': True, 'text': 'B'},
+            ]}))
+
+        with self.assertRaises(FormatError):
+            SimpleChoiceQuiz(SimpleChoiceQuiz.Source({'options': [
+                {'is_correct': False, 'text': 'A'},
+                {'is_correct': False, 'text': 'B'},
+            ]}))
 
 
-class SimpleChoiceGenerateTest(GenerateTest):
-    def specs(self):
-        return [
-            {
-                'quiz': quiz,
-                'solve': lambda dataset, clue: {
-                    'choices': [x == 'A' for x in dataset['options']]
-                }
-            }
-        ]
+class SimpleChoiceCleanReplyTest(SimpleChoiceTest):
+    def setUp(self):
+        super().setUp()
+        self.dataset = SimpleChoiceQuiz.Dataset({'options': ['A', 'B']})
+
+    def test_correct_length(self):
+        for choices in [[], [True], [True, False, False]]:
+            reply = SimpleChoiceQuiz.Reply({'choices': choices})
+            with self.assertRaises(FormatError):
+                self.quiz.clean_reply(reply, self.dataset)
+
+    def test_choices_extracted(self):
+        reply = SimpleChoiceQuiz.Reply({'choices': [True, False]})
+        clean = self.quiz.clean_reply(reply, self.dataset)
+        self.assertEqual(clean, [True, False])
 
 
-class SimpleChoiceCleanReplyTest(CleanReplyTest):
-    def specs(self):
-        return [
-            {
-                'quiz': quiz,
-                'dataset': {'options': ['A', 'B']},
-                '->': [
-                    {
-                        'reply': {'choices': [True]},
-                        'output': FormatError
-                    },
-                    {
-                        'reply': {'choices': [True, True]},
-                        'output': FormatError
-                    },
-                    {
-                        'reply': {'choices': [True, False]},
-                        'output': [True, False]
-                    },
-                ]
-            }
-        ]
+class SimpleChoiceSolveTest(SimpleChoiceTest):
+    def test_solve(self):
+        def solve(data):
+            return SimpleChoiceQuiz.Reply({
+                'choices': [x == 'A' for x in data.options]
+            })
+
+        dataset, clue = self.quiz.generate()
+        dataset = SimpleChoiceQuiz.Dataset(dataset)
+        clean_reply = self.quiz.clean_reply(solve(dataset), dataset)
+        self.assertIs(self.quiz.check(clean_reply, clue), True)
 
 
-class SimpleChoiceCheckTest(CheckTest):
-    def specs(self):
-        return [
-            {
-                'quiz': quiz,
-                'clue': [True, False],
-                '->': [
-                    {
-                        'reply': [True, False],
-                        'output': (True, '')
-                    },
-                    {
-                        'reply': [False, True],
-                        'output': (False, '')
-                    }
-                ]
-            }
-        ]
+class SimpleChoiceCheckTest(SimpleChoiceTest):
+    def test_check(self):
+        clue = [True, False]
+        self.assertIs(self.quiz.check([True, False], clue), True)
+        self.assertIs(self.quiz.check([False, True], clue), False)
