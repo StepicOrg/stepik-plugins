d01bc7b7913da4b10f4c828f2868be05da59dcb8
matklad <aleksey.kladov@gmail.com>
2014-04-03 15:00:03 +0400

    default for free-answer quiz

--- stepic_plugins/quizzes/free_answer/edit.coffee
+++ stepic_plugins/quizzes/free_answer/edit.coffee
@@ -1,3 +1,11 @@
 App.FreeAnswerQuizEditorComponent = Em.Component.extend
+  init: ->
+    @_super()
+    default_source =
+      manual_scoring: false
+
+    @set 'source',
+      @get('source') || default_source
+
   get_source: ->
     @get('source')
--- stepic_plugins/static/stepic_plugins/free-answer/edit.js
+++ stepic_plugins/static/stepic_plugins/free-answer/edit.js
@@ -1,5 +1,13 @@
 (function() {
   App.FreeAnswerQuizEditorComponent = Em.Component.extend({
+    init: function() {
+      var default_source;
+      this._super();
+      default_source = {
+        manual_scoring: false
+      };
+      return this.set('source', this.get('source') || default_source);
+    },
     get_source: function() {
       return this.get('source');
     }
