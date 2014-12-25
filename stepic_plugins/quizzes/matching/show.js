8ad889dfc3cfdbf351db323585051f412f42b738
Anton Lantsov <anton.lantsov@jetbrains.com>
2014-12-25 18:52:54 +0300

    matching ux

--- stepic_plugins/quizzes/matching/show.js
+++ stepic_plugins/quizzes/matching/show.js
@@ -20,16 +20,42 @@ function showMatchingQuiz(target, template, dataset, reply, disabled, quiz_info)
   if (!disabled) {
     pairs.on('dragstart',function(e) {
       dragSource = this;
+      $(this).addClass('matching-quiz__item-extracted');
       e.originalEvent.dataTransfer.setData('text/html', this.outerHTML);
     })
     .on('dragover',function(e) {
+      pairs.removeClass('matching-quiz__item-insert-before')
+      .removeClass('matching-quiz__item-insert-after');
+      if(pairs.index(dragSource) > pairs.index(this))
+        $(this).addClass('matching-quiz__item-insert-before')
+      else
+        $(this).addClass('matching-quiz__item-insert-after')
       e.preventDefault();
+    })
+    .on('dragend', function(e) {
+      pairs.removeClass('matching-quiz__item-insert-before')
+      .removeClass('matching-quiz__item-insert-after')
+      .removeClass('matching-quiz__item-extracted');
+    })
+    .on('dragleave', function(e) {
+      pairs.removeClass('matching-quiz__item-insert-before')
+      .removeClass('matching-quiz__item-insert-after');
     })
     .on('drop', function(e) {
       if(pairs.index(dragSource) > pairs.index(this))
         $(this).before(dragSource);
       else
         $(this).after(dragSource);
+      pairs = $(target).find('li');
+    });
+    $(target).find('.matching-quiz__arrow-up').off()
+    .on('click', function(e){
+      $(this.parentElement.previousElementSibling).before(this.parentElement);
+      pairs = $(target).find('li');
+    });
+    $(target).find('.matching-quiz__arrow-down').off()
+    .on('click', function(e){
+      $(this.parentElement.nextElementSibling).after(this.parentElement);
       pairs = $(target).find('li');
     });
   }
