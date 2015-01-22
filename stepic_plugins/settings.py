2c124f35d0dd751a156d9647d8c152afea92da46
Pavel Sviderski <pavel@psviderski.name>
2015-01-22 13:33:53 +0300

    update settings

--- stepic_plugins/settings.py
+++ stepic_plugins/settings.py
@@ -1,25 +1,23 @@
 import os
 import sys
 
-from datetime  import timedelta
+from datetime import timedelta
 
 
 PACKAGE_ROOT = os.path.dirname(os.path.dirname(__file__))
 
 RPC_TRANSPORT_URL = 'rabbit://guest:guest@localhost:5672//'
 
-# use current user and python to execute `sandboxed` code.
-SANDBOX_FOLDER = os.path.join(PACKAGE_ROOT, 'sandbox')
-SANDBOX_PYTHON_PROTO = sys.executable
-SANDBOX_REQUIREMENTS = os.path.join(PACKAGE_ROOT, 'requirements', 'sandbox.txt')
+# use current user to execute `sandboxed` code.
 SANDBOX_USER = None
+SANDBOX_FOLDER = os.path.join(PACKAGE_ROOT, 'sandbox')
+SANDBOX_PYTHON = os.path.join(SANDBOX_FOLDER, 'python', 'bin', 'python3')
+SANDBOX_JAVA = None
 SANDBOX_ENV = {
     'LANG': 'en_US.UTF-8',
 }
 ARENA_DIR = os.path.join(PACKAGE_ROOT, 'arena')
 
-SANDBOX_PYTHON = os.path.join(SANDBOX_FOLDER, 'bin', 'python3')
-SANDBOX_JAVA = None
 MB = 1024 * 1024
 JAVA_RESERVED_MEMORY = 400 * MB
 
@@ -55,7 +53,7 @@ USER_CODE_LIMITS = {
 DATASET_QUIZ_TIME_LIMIT = timedelta(minutes=5)
 DATASET_QUIZ_SIZE_LIMIT = 10 * MB
 
-# These quizzes will be scored in separate celery queue.
+# These quizzes will be scored in edy in a separate celery queue.
 COMPUTATIONALLY_HARD_QUIZZES = ['admin', 'code', 'dataset']
 
 try:
