11b686377fc2f31d2e5b06b76657f1189e77a373
Andrey Balandin <andrvb@gmail.com>
2015-03-27 19:20:08 +0300

    add numerical test settings in math problem

--- stepic_plugins/quizzes/math/__init__.py
+++ stepic_plugins/quizzes/math/__init__.py
@@ -4,13 +4,24 @@ from codejail import safe_exec
 
 from stepic_plugins.base import BaseQuiz
 from stepic_plugins.exceptions import FormatError
+from stepic_plugins.utils import parse_decimal
 
 
 class MathQuiz(BaseQuiz):
     name = 'math'
 
     class Schemas:
-        source = {'answer': str}
+        source = {
+            'answer': str,
+            'numerical_test': {
+                'z_re_min': str,
+                'z_re_max': str,
+                'z_im_min': str,
+                'z_im_max': str,
+                'max_error': str,
+                'integer_only': bool,
+            }
+        }
         reply = {'formula': str}
 
     def __init__(self, source):
@@ -18,6 +29,22 @@ class MathQuiz(BaseQuiz):
         self.answer = source.answer
         if not self.answer.strip():
             raise FormatError('Correct answer should be non-empty')
+
+        parse_float = lambda name: float(parse_decimal(getattr(source.numerical_test, name),
+                                                       'numerical_test.{}'.format(name)))
+        self.z_re_min = parse_float('z_re_min')
+        self.z_re_max = parse_float('z_re_max')
+        self.z_im_min = parse_float('z_im_min')
+        self.z_im_max = parse_float('z_im_max')
+        self.max_error = parse_float('max_error')
+        self.integer_only = source.numerical_test.integer_only
+
+        if self.z_re_min > self.z_re_max:
+            raise FormatError('Incorrect Re z')
+        if self.z_im_min > self.z_im_max:
+            raise FormatError('Incorrect Im z')
+        if self.max_error < 0:
+            raise FormatError('Incorrect tolerated absolute error')
 
     def async_init(self):
         global_dict = {'answer': self.answer}
@@ -41,18 +68,42 @@ class MathQuiz(BaseQuiz):
         global_dict = {'matched': False,
                        'hint': '',
                        'answer': self.answer,
+                       'z_re_min': self.z_re_min,
+                       'z_re_max': self.z_re_max,
+                       'z_im_min': self.z_im_min,
+                       'z_im_max': self.z_im_max,
+                       'max_error': self.max_error,
+                       'integer_only': self.integer_only,
                        'reply': reply}
         code = textwrap.dedent("""
+        from random import randint, uniform
+
+        from sympy import I, Tuple, Symbol
         from sympy.parsing.sympy_parser import parse_expr
-        from sympy.utilities.randtest import test_numerically
+        from sympy.utilities.randtest import comp
         from sympy import latex
+
+        def random_number():
+            if integer_only:
+                A, B = randint(z_re_min, z_re_max), randint(z_im_min, z_im_max)
+            else:
+                A, B = uniform(z_re_min, z_re_max), uniform(z_im_min, z_im_max)
+            return A + I*B
+
+        def test_numerically(f, g, z=None):
+            f, g, z = Tuple(f, g, z)
+            z = [z] if isinstance(z, Symbol) else (f.free_symbols | g.free_symbols)
+            reps = list(zip(z, [random_number() for zi in z]))
+            z1 = f.subs(reps).n()
+            z2 = g.subs(reps).n()
+            return comp(z1 - z2, 0, max_error)
 
         def compare(reply, answer):
             if (reply - answer).simplify() == 0:
                 return True
 
             if reply.is_Number and answer.is_Number:
-                return reply == answer
+                return abs(reply - answer) <= max_error
 
             n_tries = 3
             return all(test_numerically(reply, answer) for _ in range(n_tries))
--- stepic_plugins/utils.py
+++ stepic_plugins/utils.py
@@ -1,11 +1,13 @@
+import decimal
 import logging
 import os
 import pwd
 import shutil
 
 import bleach
-
 from codejail import jail_code
+
+from stepic_plugins.exceptions import FormatError
 
 
 logger = logging.getLogger(__name__)
@@ -117,7 +119,7 @@ ALLOWED_ATTRIBUTES = {
     'abbr': ['title'],
     'acronym': ['title'],
     'div': ['class'],
-    'img': ['src', 'alt', 'class', 'title', 'width',  'height'],
+    'img': ['src', 'alt', 'class', 'title', 'width', 'height'],
     'span': ['class'],
     'p': ['class'],
     'code': ['class']
@@ -129,3 +131,10 @@ ALLOWED_STYLES = []
 def clean_html(text):
     return bleach.clean(text, tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES,
                         styles=ALLOWED_STYLES, strip=True)
+
+
+def parse_decimal(s, filed_name):
+    try:
+        return decimal.Decimal(s.replace(',', '.').replace(' ', ''))
+    except decimal.DecimalException:
+        raise FormatError("Field `{}` should be a number".format(filed_name))
